0fb65bd1e00a39cc9d88f53442518ac2ee0e2a28
componavt <andrew.krizhanovsky@gmail.com>
2016-10-31 16:08:27 +0300

    + split sentences stub

---
+++ src/split_sentence.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+'''Function splits text to sentences.'''
+
+def splitToSentences(text):
+    print("todo...")
+
+
+def main():
+    # to read file ../data/lermontov_strange_man_part1_in.txt
+    # todo ... text = open file ...
+    
+    text_sentences = splitToSentences( text );
+
+    # write text_sentences to some file in ../data
+    # ...
+
+main()
